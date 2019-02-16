73d5218ee9f5496b417bed2ba50fe00054c6b0ba
zhaoying du <dcd0413@gmail.com>
2019-02-15 22:55:48 -0500

    4005 is great

--- R/histogram_wrapper.R
+++ R/histogram_wrapper.R
@@ -1,5 +1,5 @@
 histogram_wrapper <- function(vector,bins){
-  bins<-seq(0,max(vector)+1,bins)
-  retVal<-hist(vector,breaks=bins, plot=FALSE)
+  bin<-seq(0,max(vector)+1,bins)
+  retVal<-hist(vector,breaks=bin, plot=FALSE)
   list(frequency=retVal$counts,bins=retVal$breaks)
 }
