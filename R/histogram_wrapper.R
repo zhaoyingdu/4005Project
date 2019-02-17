a6a979cdaae2ceb2391a016f0d92f4d6bc37d94e
zhaoying du <dcd0413@gmail.com>
2019-02-16 22:36:17 -0500

    4005 is great

--- R/histogram_wrapper.R
+++ R/histogram_wrapper.R
@@ -1,6 +1,6 @@
 histogram_wrapper <- function(file,bins){
   data <- read.csv(file, header=FALSE)[[1]]
-  hist(data, breaks=bins)
+  hist(data, breaks=bins,main = paste("Histogram of" , file))
   #bin<-seq(0,max(vector)+1,bins)
   #retVal<-hist(vector,breaks=bin, plot=FALSE)
   #list(frequency=retVal$counts,bins=retVal$breaks)
